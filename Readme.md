d382421c4fe529b951f461ff10282e1879a4c892
YONGGEUN KWON <kingbbode@gmail.com>
2017-09-28 23:50:16 +0900

    start java 9 !!!

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Java 9 연습장
+
+### 해볼 것
+
+1. new Collection Utils
+
+2. new Stream Utils
+
+3. Module System
---
+++ src/test/java/com/kingbbode/java9practice/InitTest.java
@@ -0,0 +1,14 @@
+package com.kingbbode.java9practice;
+
+import org.junit.Test;
+
+/**
+ * Created by YG-MAC on 2017. 9. 28..
+ */
+public class InitTest {
+
+    @Test
+    public void 자바9를_시작해보자() {
+        System.out.println("Hello World");
+    }
+}
